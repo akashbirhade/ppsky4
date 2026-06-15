0d288c8970b980bfb4623ae2e0314a5b8f0ff07e
akashbirhade <skybirhade@gmail.com>
2026-06-16 02:07:39 +0530

    fix: update lastActive timestamps for user profiles and remove Footer component from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { LanguageProvider } from '@/context/LanguageContext'
 import { ThemeProvider } from '@/context/ThemeContext'
 import { ToastProvider } from '@/components/Toast'
 import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
 import ChatSidebar from '@/components/ChatSidebar'
 import { ChatSidebarProvider } from '@/context/ChatSidebarContext'
 import PageTransition from '@/components/PageTransition'
@@ -105,7 +104,6 @@ export default function RootLayout({
                   {children}
                 </PageTransition>
               </main>
-              <Footer />
             </div>
             <AIChatBot />
             <SideDrawer />
